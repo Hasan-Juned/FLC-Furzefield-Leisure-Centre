5c0a231c98303d386ba1f1410c917e91810ce399
Hassan <hasanahmad9812021@gmail.com>
2026-02-21 18:54:30 +0000

    main.java file created

--- FLC/src/main/java/com/mycompany/flc/FLC.java
+++ FLC/src/main/java/com/mycompany/flc/FLC.java
@@ -4,6 +4,6 @@ public class FLC {
 
     /// this is main function which is the starting point of the code entry
     public static void main(String[] args) {
-        System.out.println("Welcome to Furzefield Leisure Centre (FLC)...");
+        System.out.println("Welcome to Furzefield Leisure Centre (FLC). ..");
     }
 }
