65f7d759226f07214b25767f83a9819b523b7f9c
Hassan <hasanahmad9812021@gmail.com>
2026-03-07 12:41:57 +0000

    48 lessons' information has been created

--- FLC/src/main/java/com/mycompany/flc/LeisureCentreSystem.java
+++ FLC/src/main/java/com/mycompany/flc/LeisureCentreSystem.java
@@ -22,6 +22,34 @@ public class LeisureCentreSystem {
     public void startMenu(){
         System.out.println("System Started...");
     }
+    
+    private void createTimeTableData(){
+        String[] exerciseTypes = {"Yoga", "Zumba", "Box Fit", "Aquacise"};
+        double[] prices ={10.0, 12.0, 15.0, 11.0};
+        
+        String[] days = {"Saturday", "Sunday"};
+        String[] timeSlots = {"Morning", "Afternoon", "Evening"};
+        
+        int lessonCouter = 1;
+        
+        for(int i=1; i<=8; i++){
+            for(String day: days){
+                for (int j = 0; j < timeSlots.length; j++) {
+                    
+                    String lessonId = "L" + lessonCouter;
+                    
+                    String exercise = exerciseTypes[(lessonCouter - 1) % exerciseTypes.length];
+                    
+                    double price = prices[(lessonCouter - 1) % prices.length];
+                    
+                    Lesson lesson = new Lesson(lessonId, exercise, day +" W"+ i,timeSlots[j],price);
+                    
+                    timeTable.addLesson(lesson);
+                    lessonCouter++;
+                }
+            }
+        }
+    }
 
     /// this is main function which is the starting point of the code entry
     public static void main(String[] args) {
